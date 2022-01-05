e734394fd79729a2515c96f765c9b560b5be47b3
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-01-05 21:37:51 +0900

    add src

---
+++ src/testdrive_util.f90
@@ -0,0 +1,38 @@
+module testdrive_util
+    use :: testdrive, only:run_testsuite, testsuite_type, error_type
+    implicit none
+    private
+    public :: occurred
+    public :: run_test
+
+    character(len=*), private, parameter :: fmt = '("#", *(1x, a))'
+
+contains
+    !> utility function allowing to write `if(occurred(error))`
+    !> instead of `if(allocated(error))`
+    logical function occurred(error)
+        implicit none
+
+        type(error_type), allocatable :: error
+
+        occurred = allocated(error)
+    end function occurred
+
+    !> a subroutine running tests
+    subroutine run_test(testsuites)
+        use, intrinsic :: iso_fortran_env, only: error_unit
+        implicit none
+        type(testsuite_type) :: testsuites(:)
+        integer :: stat, is
+
+        do is = 1, size(testsuites)
+            write (error_unit, fmt) "Testing:", testsuites(is)%name
+            call run_testsuite(testsuites(is)%collect, error_unit, stat)
+        end do
+
+        if (stat > 0) then
+            write (error_unit, '(i0, 1x, a)') stat, "test(s) failed!"
+            error stop
+        end if
+    end subroutine run_test
+end module testdrive_util
