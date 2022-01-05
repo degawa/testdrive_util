34dd6fd32328e4ce3ff0584311cf9927988d32a0
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-01-05 21:38:24 +0900

    add tests

---
+++ test/test_occurred.f90
@@ -0,0 +1,27 @@
+program test_occurred
+    use, intrinsic :: iso_fortran_env, only: error_unit, int32
+    use testdrive, only: run_testsuite, new_testsuite, testsuite_type
+    use :: unittests_occurred, only:collect_test_occurred
+    implicit none
+
+    integer(int32) :: stat, is
+    type(testsuite_type), allocatable :: testsuites(:)
+    character(len=*), parameter :: fmt = '("#", *(1x, a))'
+
+    stat = 0
+
+    testsuites = [ &
+                 new_testsuite("test suite for function occurred", &
+                               collect_test_occurred) &
+                 ]
+
+    do is = 1, size(testsuites)
+        write (error_unit, fmt) "Testing:", testsuites(is)%name
+        call run_testsuite(testsuites(is)%collect, error_unit, stat)
+    end do
+
+    if (stat > 0) then
+        write (error_unit, '(i0, 1x, a)') stat, "test(s) failed!"
+        error stop
+    end if
+end program test_occurred
---
+++ test/unittest_occurred.f90
@@ -0,0 +1,36 @@
+module unittests_occurred
+    use :: testdrive, only:new_unittest, unittest_type, error_type, check
+    use :: testdrive_util, only:occurred
+    implicit none
+    private
+    public :: collect_test_occurred
+
+contains
+    subroutine collect_test_occurred(testsuite)
+        implicit none
+        type(unittest_type), allocatable, intent(out) :: testsuite(:)
+        testsuite = [new_unittest("it returns true when argument 'error' is allocated", &
+                                  test_occurd_returns_true_when_error_is_allocated), &
+                     new_unittest("it returns false when argument 'error' is not allocated", &
+                                  test_occurd_returns_false_when_error_is_not_allocated) &
+                     ]
+    end subroutine collect_test_occurred
+
+    subroutine test_occurd_returns_true_when_error_is_allocated(error)
+        implicit none
+        type(error_type), allocatable, intent(out) :: error
+        type(error_type), allocatable :: err_allocated
+
+        allocate (err_allocated)
+        call check(error, actual=occurred(err_allocated), expected=.true.)
+    end subroutine test_occurd_returns_true_when_error_is_allocated
+
+    subroutine test_occurd_returns_false_when_error_is_not_allocated(error)
+        implicit none
+        type(error_type), allocatable, intent(out) :: error
+        type(error_type), allocatable :: err_not_allocated
+
+        if (allocated(err_not_allocated)) deallocate (err_not_allocated)
+        call check(error, actual=occurred(err_not_allocated), expected=.false.)
+    end subroutine test_occurd_returns_false_when_error_is_not_allocated
+end module unittests_occurred
