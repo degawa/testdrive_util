7b5acc0f1935f326aff0c2262940024de39ce9ae
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-03-21 20:25:38 +0900

    added `get_all_actual_value`
    to read scratch file from beginning to EOF

--- src/testdrive_util.f90
+++ src/testdrive_util.f90
@@ -7,6 +7,7 @@ module testdrive_util
     public :: run_test
     public :: to_string
     public :: get_actual_value
+    public :: get_all_actual_value
 
     character(len=*), private, parameter :: fmt = '("#", *(1x, a))'
 
@@ -17,7 +18,10 @@ module testdrive_util
     end interface
 
     interface get_actual_value
-        procedure :: get_actual_value_from_unit_str
+    end interface
+
+    interface get_all_actual_value
+        procedure :: get_actual_value_from_beginning_to_eof_as_str_from_unit
     end interface
 
 contains
@@ -104,5 +108,64 @@ contains
         end if
 
         string = trim(buffer)
-    end subroutine get_actual_value_from_unit_str
+    !>get value from beginning to EOF
+    !>from unit specified by `unit_number`
+    !>and stores it in `string`.
+    !>
+    !>allocate `error` if an error occurs.
+    !>
+    !>The unit specified by `unit_number` must be open.
+    !>
+    !>@note `string` is allocated as 0 length string if the unit is empty
+    !>
+    !>@warning this procedure cannot read tailing spaces.
+    subroutine get_actual_value_from_beginning_to_eof_as_str_from_unit(error, unit_number, string)
+        implicit none
+        type(error_type), allocatable, intent(out) :: error
+        integer(int32), intent(in) :: unit_number
+            !! opened unit number containing actual value
+        character(:), allocatable, intent(inout) :: string
+
+        logical :: opened
+        integer(int32) :: iostat
+        character(default_buffer_length) :: buffer
+
+        inquire (unit=unit_number, opened=opened, iostat=iostat)
+        call check(error, opened, &
+                   "unit "//to_string(unit_number)//" is not opened.")
+        if (occurred(error)) return
+
+        rewind (unit=unit_number, iostat=iostat)
+        call check(error, iostat == 0, &
+                   "io error "//to_string(iostat)// &
+                   " occured while rewinding the unit.")
+        if (occurred(error)) return
+
+        read_to_eof: do while (.true.)
+            read (unit_number, '(A)', iostat=iostat) buffer
+            if (is_iostat_end(iostat)) then
+                exit read_to_eof
+            else
+                call check(error, iostat == 0, &
+                           "io error "//to_string(iostat)// &
+                           " occured while reading actual value from the unit.")
+                if (occurred(error)) return
+            end if
+
+            call append_with_newline(string, buffer)
+        end do read_to_eof
+
+        if (.not. allocated(string)) string = ""
+    contains
+        subroutine append_with_newline(base, suffix)
+            character(:), allocatable, intent(inout) :: base
+            character(*), intent(in) :: suffix
+
+            if (.not. allocated(base)) then
+                base = trim(suffix)
+            else
+                base = base//new_line("a")//trim(buffer)
+            end if
+        end subroutine append_with_newline
+    end subroutine get_actual_value_from_beginning_to_eof_as_str_from_unit
 end module testdrive_util
